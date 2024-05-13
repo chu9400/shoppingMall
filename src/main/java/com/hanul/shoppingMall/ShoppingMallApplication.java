e046cd5619642c3c20961c738d6e65aac30bcf1a
Hanul <chu9400@naver.com>
2024-05-14 07:45:36 +0900

    update - @PropertySource 사용하여 properties 정보 관리

--- src/main/java/com/hanul/shoppingMall/ShoppingMallApplication.java
+++ src/main/java/com/hanul/shoppingMall/ShoppingMallApplication.java
@@ -2,8 +2,10 @@ package com.hanul.shoppingMall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-variable.properties")
 public class ShoppingMallApplication {
 
 	public static void main(String[] args) {
